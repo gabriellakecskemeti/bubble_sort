58a31de8abb7513c99fd8e4e35344fcf316bd35e
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2022-02-08 19:54:03 +0100

    TC6,TC7 testcases out of the exercise

--- bubble_sort_test.py
+++ bubble_sort_test.py
@@ -6,7 +6,10 @@ _test_data_bubble_sort = [
     pytest.param([8], [8], id="TC2: Exc10, 1 item"),
     pytest.param([2, 9], [2, 9], id="TC3: Exc10, 2 items sorted"),
     pytest.param([9, 2], [2, 9], id="TC4: Exc10, 2 items mixed"),
-    pytest.param([3, 7, 1, 6, 4, 2, 5, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], id="TC5: Exc9, statement coverage 100%")
+    pytest.param([3, 7, 1, 6, 4, 2, 5, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], id="TC5: Exc9, statement coverage 100%"),
+    pytest.param(["peter", "Pal", "Evi", "michael", "Roxanne", "eva", "Eva"],
+                 ["Eva", "Evi", "Pal", "Roxanne", "eva", "michael", "peter"], id="TC6: characters"),
+    pytest.param([9.012345, 2.02], [2.02, 9.012345], id="TC7: float")
 ]
 
 
